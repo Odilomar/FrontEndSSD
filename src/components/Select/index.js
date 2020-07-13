5e0b500f3212a5a41a411e91cd7109b5d1bab2a1
Odilomar Rebelo Rocha Junior <odilomar.junior@gmail.com>
2020-07-13 10:53:18 -0400

    :art: Select input changed

---
+++ src/components/Select/index.js
@@ -0,0 +1,28 @@
+// import React from "react";
+// import Select from "react-select";
+
+// export default Props =>
+// <>
+//     <Select options = {Props.options}
+//             placeholder = {Props.placeholder}
+//             onChange = />
+// </>
+
+
+// // const options = [
+// //     { value: "admin", label: "Admin" },
+// //     { value: "super", label: "Super" },
+// //     { value: "tecnico", label: "Técnico" },
+// // // ];
+
+// // const ReactSelectComponent = (props) => {
+// //     const handleSelectChange = (selectedOption) => {
+// //         console.log(selectedOption);
+// //     };
+
+// //     return (
+// //         < Select options = { options } onChange={ this.handleSelectChange() }/>
+// //     );
+// // };
+
+// // export default ReactSelectComponent;
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,67 +1,92 @@
-import React, { Component } from 'react';
-import { Form, FormGroup, Label, Input, Button, Alert } from 'reactstrap';
+import React, { Component } from "react";
+import { Form, FormGroup, Label, Input, Button, Alert } from "reactstrap";
 
-import Header from '../../components/Header';
+import Header from "../../components/Header";
+// import ReactSelectComponent from "../../components/Select";
+import Select from "react-select";
 
 export default class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: this.props.location.state
+        ? this.props.location.state.message
+        : "",
+    };
+  }
+  signIn = () => {
+    const data = { nome: this.nome, role: this.role };
+    const requestInfo = {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: new Headers({
+        "Content-Type": "application/json",
+      }),
+    };
 
-    constructor(props) {
-        super(props)
-        this.state = {
-            message : this.props.location.state?this.props.location.state.message: '',
-        };
-    }
+    fetch("https://localhost:44307/v1/account/login", requestInfo)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Login inválido...");
+      })
+      .then((token) => {
+        localStorage.setItem("token", token);
+        this.props.history.push("/admin");
+        return;
+      })
+      .catch((e) => {
+        this.setState({ message: e.message });
+      });
+  };
 
-    signIn = () => {
-        const data = { nome: this.nome, role: this.role };
-        const requestInfo = {
-            method: 'POST',
-            body: JSON.stringify(data),
-            headers: new Headers({
-                'Content-Type': 'application/json'
-            }),
-        };
+  handleSelectChange = (selectedOption) => {
+    console.log(selectedOption);
+  };
 
-        fetch('https://localhost:44307/v1/account/login', requestInfo)
-        .then(response => {
-            if(response.ok) {
-                return response.json()
-            }
-            throw new Error("Login inválido...");
-        })
-        .then(token => {
-            localStorage.setItem('token', token);
-            this.props.history.push("/admin");
-            return;
-        })
-        .catch(e => {
-            this.setState({ message: e.message });
-        }); 
-    }
+  render() {
+    return (
+      <div className="col-md-6">
+        <Header title="Sistema de Segurança Digital" />
 
-    render() {
-        return (
-            <div className="col-md-6">
-                <Header title="Sistema de Segurança Digital" />
-
-                <hr  className="my-3"/>
-                {
-                    this.state.message !== ''? (
-                        <Alert color="danger" className="text-center"> {this.state.message} </Alert>
-                    ) : ''
-                }
-                <Form>
-                    <FormGroup>
-                        <Label for="nome">Nome</Label>
-                        <Input type="text" id="nome" onChange={e => this.nome = e.target.value} placeholder="Informe seu nome" />
-                    </FormGroup>
-                    <FormGroup>
-                        <Label for="role">Setor</Label>
-                        <Input type="text" id="role" onChange={e => this.role = e.target.value} placeholder="Informe o setor" />
-                    </FormGroup>
-                    <Button color="primary" block onClick={this.signIn}> Entrar </Button>
-                </Form>
-            </div>
-        );
-    }
+        <hr className="my-3" />
+        {this.state.message !== "" ? (
+          <Alert color="danger" className="text-center">
+            {" "}
+            {this.state.message}{" "}
+          </Alert>
+        ) : (
+          ""
+        )}
+        <Form>
+          <FormGroup>
+            <Label for="nome">Nome</Label>
+            <Input
+              type="text"
+              id="nome"
+              onChange={(e) => (this.nome = e.target.value)}
+              placeholder="Informe seu nome"
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label for="role">Setor</Label>
+            <Select
+              options={[
+                { value: "admin", label: "Administrador do Sistema" },
+                { value: "super", label: "Super Usuário" },
+                { value: "tecnico", label: "Técnico" },
+              ]}
+              placeholder="Selecione aqui"
+              onChange={(e) => (this.role = e.value)}
+            />
+          </FormGroup>
+          <Button color="primary" block onClick={this.signIn}>
+            {" "}
+            Entrar{" "}
+          </Button>
+        </Form>
+      </div>
+    );
+  }
 }
